82c1fd618daca14ae6132af3048b55eaa20e6815
Anthony Bonarrigo <abon219@gmail.com>
2015-07-22 08:58:22 -0400

    Create ProjectEuler-1.java

---
+++ ProjectEuler-1.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int numOfTests = input.nextInt();
+        int currentTest = 1;
+        
+        while(currentTest <= numOfTests){
+            int number = input.nextInt();
+            for (int i = 0; i < number; i++){
+                if(i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            System.out.println(sum);
+            currentTest++;
+            //reset sum
+            sum = 0;
+        }
+    }
+}
