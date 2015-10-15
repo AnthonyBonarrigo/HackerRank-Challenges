180b839f05a89a7b01719affa5fcc2c7275ef77b
Anthony Bonarrigo <abon219@gmail.com>
2015-10-15 18:06:57 -0400

    Create PreorderTraversal.java

---
+++ PreorderTraversal.java
@@ -0,0 +1,12 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+void Preorder(Node root) {
+    if(root == null){return;}
+    System.out.print(root.data + " ");
+    Preorder(root.left);
+    Preorder(root.right);
+}
