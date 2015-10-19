8fefedfcecf33f0a8bbfdcb9a9cb409d318b6081
Anthony Bonarrigo <abon219@gmail.com>
2015-10-19 17:20:49 -0400

    Update ArrayReverse.java

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -7,7 +7,9 @@ import java.util.regex.*;
 public class ArrayReverse{
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. 
+           Input format: The first line of input contains N where N is the number of integers
+           The second line contains N integers                                              */
         Scanner input = new Scanner(System.in);
         int numOfTerms = input.nextInt();
         int[] array = new int[numOfTerms];
